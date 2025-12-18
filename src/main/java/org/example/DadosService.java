f988b8a8989c3e67894530dd53a2a966240ae15f
edson66 <edsonulissesr1@gmail.com>
2025-12-17 23:41:02 -0300

    excel substituindo os placeholders

--- src/main/java/org/example/DadosService.java
+++ src/main/java/org/example/DadosService.java
@@ -3,32 +3,75 @@ package org.example;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DadosService {
 
-    private static Map<String,String> lerArquivoDoador(String caminho) throws FileNotFoundException {
+    private static final DecimalFormat df = new DecimalFormat("#,##0.00");
+
+    public static Map<String,String> lerArquivoDoador(String caminho) throws FileNotFoundException {
         Map<String,String> dados = new HashMap<>();
 
         try(FileInputStream inputStream = new FileInputStream(new File(caminho));
             Workbook workbook = new XSSFWorkbook(inputStream)) {
             Sheet sheet = workbook.getSheetAt(0);
-            Row row = sheet.getRow(0);
 
-            dados.put("<ENDERECO>",row.getCell(0).getStringCellValue());
+            int linhaAtual = 0;
+            int contadorItens = 1;
 
-            return dados;
+            while (true){
+                Row row = sheet.getRow(linhaAtual);
+
+                if (row == null || isCellEmpty(row.getCell(1))){
+                    break;
+                }
+
+                String keyItens = "<ITEM" + String.valueOf(contadorItens) + ">";
+                dados.put(keyItens,row.getCell(0).getStringCellValue());
+
+                String keyUn = "<UN" + String.valueOf(contadorItens) + ">";
+                dados.put(keyUn,row.getCell(1).getStringCellValue());
+
+                String keyQt = "<QT" + String.valueOf(contadorItens) + ">";
+                dados.put(keyQt,String.valueOf((int) row.getCell(2).getNumericCellValue()));
+
+                String keyValor = "<VALOR" + String.valueOf(contadorItens) + ">";
+                dados.put(keyValor,lerComoDinheiro(row.getCell(3)));
+
+                linhaAtual++;
+                contadorItens++;
+            }
+
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        return dados;
     }
 
-    private static void preencherTemplate(String caminhoEntrada,String caminhoSaida,Map<String,String> dados){
+    private static String lerComoDinheiro(Cell cell) {
+        if (cell == null) return "0,00";
+
+        if (cell.getCellType() == CellType.NUMERIC) {
+            double valor = cell.getNumericCellValue();
+            return df.format(valor);
+        }else{
+            return cell.getStringCellValue().replace(".", ",");
+        }
+    }
+
+    private static boolean isCellEmpty(Cell cell) {
+        return cell == null || cell.getCellType() == CellType.BLANK
+                || (cell.getCellType() == CellType.STRING && cell.getStringCellValue().trim().isEmpty());
+    }
+
+
+    public static void preencherTemplate(String caminhoEntrada,String caminhoSaida,Map<String,String> dados){
         try(FileInputStream inputStream = new FileInputStream(new File(caminhoEntrada));
             Workbook workbook = new XSSFWorkbook(inputStream)) {
 
@@ -38,13 +81,20 @@ public class DadosService {
                         if (cell.getCellType() == CellType.STRING){
                             String valorAtual = cell.getStringCellValue();
 
-                            for (Map<String,String> dados: ){
-
+                            if (dados.containsKey(valorAtual)) {
+                                cell.setCellValue(dados.get(valorAtual));
                             }
+
                         }
                     }
                 }
             }
+
+            try (FileOutputStream out = new FileOutputStream(new File(caminhoSaida))) {
+                workbook.write(out);
+            }
+            System.out.println("Arquivo gerado com sucesso: " + caminhoSaida);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,17 +8,17 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        File arquivo = new File("src/main/resources/tabela.xlsx");
+    public static void main(String[] args) throws FileNotFoundException {
+        Map<String,String> dados = DadosService.lerArquivoDoador("src/main/resources/tabela3.xlsx");
 
-        try{
-            Map<String,String> dadosParaSubstituir = new HashMap<>()
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+
+        DadosService.preencherTemplate("src/main/resources/NCE_Modelo.xlsx",
+                "src/main/resources/NCE.xlsx",dados);
     }
 }
